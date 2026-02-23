15a01a5cd9ad1238206538558c019dd264252a92
David Sarkies <david.sarkies@internode.on.net>
2026-02-23 20:15:23 +1100

    Add failed tie command

--- commands/Tie.java
+++ commands/Tie.java
@@ -12,6 +12,7 @@ package commands;
 
 import command_process.ActionResult;
 import command_process.ParsedCommand;
+import data.GameEntities;
 import game.Game;
 import game.Player;
 
@@ -45,10 +46,29 @@ public class Tie {
      * @return an {@link ActionResult} describing validity and effects
      */
 	public ActionResult executeTie() {
+		game.addMessage("You are unable to do that.", false, false);
 		ActionResult result = new ActionResult(game,player,false);
+		
+		if (isTyingSheetOrRope(command.getNounNumber(),player.getRoom(),game)) {
+			result = tyingSheetOrRope(game,player);
+		}
+		
 		return result;
 	}
-	//2010 IF B=2 OR B=14 THEN R$="NOTHING TO TIE IT TO!"
+	
+	private boolean isTyingSheetOrRope(int nounNumber,int roomNumber,Game game) {
+		return ((nounNumber == GameEntities.ITEM_SHEET && 
+				game.getItem(GameEntities.ITEM_SHEET).getItemLocation() == GameEntities.ROOM_CARRYING) ||
+				(nounNumber == GameEntities.ITEM_ROPE && 
+				game.getItem(GameEntities.ITEM_ROPE).getItemLocation() == GameEntities.ROOM_CARRYING)) &&
+				roomNumber != GameEntities.ROOM_WELL;
+	}
+	
+	private ActionResult tyingSheetOrRope(Game game,Player player) {
+		game.addMessage("Nothing to tie it to!", true, false);
+		return new ActionResult(game,player,true);
+	}
+	
 	//2020 IF H=7214 THEN R$="IT IS TIED": C(14)=72: F(53)=1
 	//2030 IF H=722 THEN R$="OK": F(40)=1: C(2)=72
 }
