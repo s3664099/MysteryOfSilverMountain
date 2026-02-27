4425d4384dc2119c97ba9a5695c0c4e4e26e4d6a
David Sarkies <david.sarkies@internode.on.net>
2026-02-27 21:10:30 +1100

    Started climb actions

--- commands/Climb.java
+++ commands/Climb.java
@@ -2,8 +2,8 @@
 Title: Mystery of Silver Mountain Climb Class
 Author: Chris Oxlade & Judy Tatchell
 Translator: David Sarkies
-Version: 1.0
-Date: 25 February 2026
+Version: 1.1
+Date: 27 February 2026
 Source: https://archive.org/details/the-mystery-of-silver-mountain/mode/2up
 */
 
@@ -50,8 +50,8 @@ public class Climb {
 		
 		if (isClimbVine(player.getRoom(),command.getNounNumber(),game)) {
 			result = climbVine(game,player);
-		} else if (isNotAttached(game,command.getNounNumber()) {
-			result = notAttached();
+		} else if (isNotAttached(game,command.getNounNumber(),player.getRoom())) {
+			result = notAttached(game,player);
 		} else if (isAtTheTop(game,command.getNounNumber(),player.getRoom())) {
 			result = atTheTop(game,player);
 		} else if (isGoingDown(game,command.getNounNumber(),player.getRoom())) {
@@ -76,13 +76,47 @@ public class Climb {
 		return new ActionResult(game,player,true);
 	}
 	
+	private boolean isNotAttached(Game game,int nounNumber,int roomNumber) {
+		return (nounNumber== GameEntities.ITEM_ROPE && 
+				(game.getItem(GameEntities.ITEM_ROPE).getItemLocation() == GameEntities.ROOM_CARRYING ||
+				game.getItem(GameEntities.ITEM_ROPE).getItemLocation() == roomNumber)) ||
+				(nounNumber== GameEntities.ITEM_SHEET && 
+				(game.getItem(GameEntities.ITEM_ROPE).getItemLocation() == GameEntities.ROOM_CARRYING ||
+				game.getItem(GameEntities.ITEM_ROPE).getItemLocation() == roomNumber)) &&
+				(roomNumber != GameEntities.ROOM_PINNACLE && roomNumber != GameEntities.ROOM_WELL &&
+				roomNumber != GameEntities.ROOM_WELL_BOTTOM);
+	}
+	
+	private ActionResult notAttached(Game game,Player player) {
+		game.addMessage("Not attached to anything!", true, false);
+		return new ActionResult(game,player,true);
+	}
+	
+	private boolean isAtTheTop(Game game,int nounNumber,int roomNumber) {
+		return nounNumber == GameEntities.ITEM_ROPE && roomNumber == GameEntities.ROOM_PINNACLE &&
+				game.getItem(GameEntities.ITEM_ROPE).getItemLocation() == GameEntities.ROOM_PINNACLE;
+	}
+	
+	private ActionResult atTheTop(Game game,Player player) {
+		game.addMessage("You are at the top!", true, false);
+		return new ActionResult(game,player,true);
+	}
+	
+	private boolean isGoingDown(Game game,int nounNumber,int roomNumber) {
+		return nounNumber == GameEntities.ITEM_ROPE && roomNumber == GameEntities.ROOM_WELL &&
+				game.getItem(GameEntities.FLAG_ROPE_TIED).getItemFlag() == 1;
+	}
+	
+	private ActionResult goingDown(Game game,Player player) {
+		game.addMessage("Going down.", true, false);
+		player.setRoom(GameEntities.ROOM_WELL_BOTTOM);
+		return new ActionResult(game,player,true);
+	}
 
-	//2060 IF B=14 OR B=2 THEN R$="NOT ATTACHED TO ANYTHING!"
-	//2070 IF H=5414 AND C(14)=54 THEN R$="YOU ARE AT THE TOP"
-	//2080 IF H=7214 AND F(53)=1 THEN R$="GOING DOWN": R=71
 	//2090 IF H=722 AND F(4O)=1 THEN R=71: R$="IT IS TORN": C(2)=81: F(40)=0
 	//2100 IF H=7114 AND F(53)=1 THEN C(14)=71: F(53)=0: R$="IT FALLS DOWN-BUMP!"	
 }
 
 /* 25 February 2026 - Created file
+ * 27 February 2026 - Added branches and started climb actions
  */
