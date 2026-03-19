4e2ca7520c0091d2965259334d501b59884cbd67
David Sarkies <david.sarkies@internode.on.net>
2026-03-19 21:18:32 +1100

    Added responses for empty

--- commands/Empty.java
+++ commands/Empty.java
@@ -2,8 +2,8 @@
 Title: Mystery of Silver Mountain Empty Class
 Author: Chris Oxlade & Judy Tatchell
 Translator: David Sarkies
-Version: 1.0
-Date: 15 March 2026
+Version: 1.1
+Date: 19 March 2026
 Source: https://archive.org/details/the-mystery-of-silver-mountain/mode/2up
 */
 
@@ -11,6 +11,7 @@ package commands;
 
 import command_process.ActionResult;
 import command_process.ParsedCommand;
+import data.GameEntities;
 import game.Game;
 import game.Player;
 
@@ -47,11 +48,31 @@ public class Empty {
 		game.addMessage("You cannot Empty that", true, false);
 		ActionResult result = new ActionResult(game,player,true);
 		
+		if isWater(command.getNounNumber()) {
+			result = new Water(game,player,command).executeWater();
+		} else if isEmptyPhial(game,player.getRoom(),command.getNounNumber()) {
+			result = emptyPhial(game,player);
+		}
+		
 		return result;
 	}
 	
-	//2520 IF B=16 THEN B=22:GOSUB 2450 - Executes a water
-	//2530 IF H=499 THEN R$="WHERE?"
+	private boolean isWater(int nounNumber) {
+		return nounNumber == GameEntities.ITEM_SEEDS ||
+				nounNumber == GameEntities.ITEM_JUG;
+	}
+	
+	private boolean isEmptyPhial(Game game, int roomNumber, int nounNumber) {
+		return game.getItem(GameEntities.ITEM_PHIAL).getItemLocation() == GameEntities.CARRYING &&
+				roomNumber == GameEntities.ROOM_MOSAIC_HALL && nounNumber == GameEntities.ITEM_PHIAL;
+	}
+	
+	private ActionResult emptyPhial(Game game,Player player) {
+		game.addMessage("Where?", true, false);
+		return new ActionResult(game,player,true);
+	}
 }
 /* 15 March 2026 - Created class
+ * 19 March 2026 - Added the responses
+ */
 */
