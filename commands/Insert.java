0e9976f0dd0e5c1819846439f40b26e430aa9a62
David Sarkies <david.sarkies@internode.on.net>
2026-04-11 16:09:07 +1000

    Added Javadocs to insert command

--- commands/Insert.java
+++ commands/Insert.java
@@ -11,7 +11,6 @@ package commands;
 
 import command_process.ActionResult;
 import command_process.ParsedCommand;
-import data.Constants;
 import data.GameEntities;
 import game.Game;
 import game.Player;
@@ -57,22 +56,51 @@ public class Insert {
 		return result;
 	}
 	
+	/**
+	 * Returns true if the player has no coins
+	 * 
+ 	 * @param nounNumber the value of the noun entered
+     * @param game the current game state
+	 * @return boolean
+	 */
 	private boolean isInsertCoinsNone(Game game, int nounNumber) {
 		return nounNumber == GameEntities.ITEM_COINS &&
 				game.getItem(GameEntities.FLAG_COIN_NUMBERS).getItemFlag() == 0;
 	}
 	
+    /**
+     * Alerts the player that they have no coins left
+     *
+     * @param game the current game state
+     * @param player the player making the move
+     * @return an {@link ActionResult} describing the outcome
+     */
 	private ActionResult insertCoinsNone(Game game, Player player) {
 		game.addMessage("You do not have any", false, false);
 		return new ActionResult(game,player,true);
 	}
 	
+	/**
+	 * Returns true if the player is inserting coins in the tower
+	 * 
+	 * @param roomNumber the room the player is in
+ 	 * @param nounNumber the value of the noun entered
+     * @param game the current game state
+	 * @return boolean
+	 */
 	private boolean isInsertCoinInTurret(Game game,int roomNumber, int nounNumber) {
 		return roomNumber == GameEntities.ROOM_TURRET && nounNumber == GameEntities.ITEM_COIN &&
 				game.getItem(GameEntities.FLAG_COIN_NUMBERS).getItemFlag()>0 &&
 				game.getItem(GameEntities.ITEM_COINS).getItemLocation() == GameEntities.ROOM_CARRYING;
 	}
 	
+    /**
+     * Executes the insert coin action in the tower
+     *
+     * @param game the current game state
+     * @param player the player making the move
+     * @return an {@link ActionResult} describing the outcome
+     */
 	private ActionResult insertCoinInTurrent(Game game, Player player) {
 		int numberOfCoins = game.getItem(GameEntities.FLAG_COIN_NUMBERS).getItemFlag();
 		game.getItem(GameEntities.FLAG_COIN_NUMBERS).setItemFlag(numberOfCoins--);
@@ -83,6 +111,12 @@ public class Insert {
 		return new ActionResult(game,player,true);
 	}
 	
+	/**
+	 * Returns true if the player has not inserted the last coin
+	 * 
+ 	 * @param numberOfCoins the number of coins the player has
+	 * @return boolean
+	 */
 	private boolean noCoinsLeft(int numberOfCoins) {
 		return GameEntities.FLAG_COIN_NUMBERS>0;
 	}
@@ -91,4 +125,5 @@ public class Insert {
 /* 9 April 2026 - Created file
  * 10 April 2026 - Added insert coins when you have none
  * 11 April 2026 - Added insert coins in turret
+ * 				 - Added Javadocs
  */
