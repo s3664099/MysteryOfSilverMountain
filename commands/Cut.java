a2ca2a412975a955be405cd518e5e09f42e540f0
David Sarkies <david.sarkies@internode.on.net>
2026-04-22 20:44:13 +1000

    Added response in cut

--- commands/Cut.java
+++ commands/Cut.java
@@ -2,8 +2,8 @@
 Title: Mystery of Silver Mountain Cut Class
 Author: Chris Oxlade & Judy Tatchell
 Translator: David Sarkies
-Version: 1.0
-Date: 20 April 2026
+Version: 1.1
+Date: 22 April 2026
 Source: https://archive.org/details/the-mystery-of-silver-mountain/mode/2up
 */
 
@@ -11,6 +11,7 @@ package commands;
 
 import command_process.ActionResult;
 import command_process.ParsedCommand;
+import data.GameEntities;
 import game.Game;
 import game.Player;
 
@@ -46,12 +47,19 @@ public class Cut {
 	public ActionResult executeCut() {
 		game.addMessage("How?", true, false);
 		ActionResult result = new ActionResult(game,player,true);
-				
+		
+		if (isCutWebs(player.getRoom(),command.getNounNumber())) {
+			result = new Swing(game,player,command).executeSwing();
+		}
 		return result;
 	}
 	
-	//3050 IF H=5861 THEN H=5818:GOSUB 2470
+	private boolean isCutWebs(int roomNumber, int nounNumber) {
+		return roomNumber == GameEntities.ROOM_COBWEB &&
+				nounNumber == GameEntities.ITEM_COBWEBS;
+	}
 }
 
-/* 30 April 2026 - Created File
+/* 21 April 2026 - Created File
+ * 22 April 2026 - Added response & Javadocs
  */
