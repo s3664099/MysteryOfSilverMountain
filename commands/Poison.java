9508e046beb0f15ad282831e3c3a7caf4e1913f2
David Sarkies <david.sarkies@internode.on.net>
2026-05-03 13:58:06 +1000

    Added poison action

--- commands/Poison.java
+++ commands/Poison.java
@@ -2,8 +2,8 @@
 Title: Mystery of Silver Mountain Poison Class
 Author: Chris Oxlade & Judy Tatchell
 Translator: David Sarkies
-Version: 1.0
-Date: 1 May 2026
+Version: 1.1
+Date: 3 May 2026
 Source: https://archive.org/details/the-mystery-of-silver-mountain/mode/2up
 */
 
@@ -22,7 +22,10 @@ public class Poison {
 	
     /** The active player instance. */
 	private final Player player;
-		
+
+    /** The current command instance. */
+	private final ParsedCommand command;
+	
     /**
      * Creates a {@code Poison} handler for executing a poison action
      * 
@@ -33,6 +36,7 @@ public class Poison {
 	public Poison(Game game, Player player, ParsedCommand command) {
 		this.game = game;
 		this.player = player;
+		this.command = command;
 	}
 	
 	 /**
@@ -41,13 +45,42 @@ public class Poison {
      * @return an {@link ActionResult} describing validity and effects
      */
 	public ActionResult executePoison() {
-		game.addMessage("You are unable to do that.", false, false);
+		game.addMessage("That is not possible.", false, false);
 		ActionResult result = new ActionResult(game,player,false);
-	
+		
+		if (isPoisonWine(game,player.getRoom(),command.getNounNumber())) {
+			result = poisonWine(game,player);
+		}
 		return result;
 	}
-	//2990 IF (B=67 OR B=68) AND C(9)=0 AND R=49 THEN R$="OK": F(47)=1
+	
+	/**
+	 * Returns true if the player is poisoning the wine
+	 * 
+ 	 * @param nounNumber the value of the noun entered
+ 	 * @param roomNumber the room the player is in
+	 * @return boolean
+	 */
+	private boolean isPoisonWine(Game game, int roomNumber, int nounNumber) {
+		return (nounNumber == GameEntities.ITEM_WINE || nounNumber == GameEntities.ITEM_GOBLET) &&
+				roomNumber == GameEntities.ROOM_MOSAIC_HALL && 
+				game.getItem(GameEntities.ITEM_PHIAL).getItemLocation() == GameEntities.ROOM_CARRYING;
+	}
+	
+    /**
+     * Executes a response if the player is poisoning the wine
+     *
+     * @param game the current game state
+     * @param player the player making the move
+     * @return an {@link ActionResult} describing the outcome
+     */
+	private ActionResult poisonWine(Game game, Player player) {
+		game.addMessage("OK", true, false);
+		game.getItem(GameEntities.FLAG_WINE_POISONED).setItemFlag(1);
+		return new ActionResult(game,player,true);
+	}
 }
 
 /* 1 May 2026 - Created File
+ * 3 May 2026 - Added responses
  */
